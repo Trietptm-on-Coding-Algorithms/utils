9ab608ece114588796b990a3789aa16b56ee56e8
Evan Teitelman <teitelmanevan@gmail.com>
2013-01-05 21:05:54 -0500

    added last-used and seq-rename scripts
    modified tar-rootfs script to exclude SWAP.swap

--- tar-rootfs.sh
+++ tar-rootfs.sh
@@ -1,1 +1,1 @@
-/bin/tar -cvpzf "$2" --exclude=proc --exclude=lost+found --exclude=media --exclude=mnt --exclude=sys "$1"
+/bin/tar -cvpzf "$2" --exclude=proc --exclude=lost+found --exclude=SWAP.swap --exclude=media --exclude=mnt --exclude=sys "$1"
