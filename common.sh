3e3c011fbba4b90565cdfc1677a149fa4a42213c
Evan Teitelman <teitelmanevan@gmail.com>
2013-06-05 10:23:04 -0400

    common.sh: Fix a bug in 'needs'.

--- common.sh
+++ common.sh
@@ -9,13 +9,12 @@ die() {
 }
 
 needs() {
-	local item
 	local extra_error
 	while [[ -n $1 ]] ; do
 		case "$1" in
 			-e) extra_error="$2" ;  shift ;;
 			root) (( $UID == 0 )) || die "Must be root. $extra_error" ;;
-			*) command -v "$item" &>/dev/null || die "Needs $item. $extra_error" ;;
+			*) command -v "$1" &>/dev/null || die "Needs $1. $extra_error" ;;
 		esac
 		shift
 	done
