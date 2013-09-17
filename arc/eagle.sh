2cb419fd55b29f6574c61e8ba08c5966bac26b71
Evan Teitelman <teitelmanevan@gmail.com>
2013-09-17 12:20:59 -0400

    Remove the `arc` directory.
    Why was this even here?

--- arc/eagle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Eagle/6.1/bin/eagle.exe >/dev/null 2>&1
-exit
--- arc/grade.pl
+++
@@ -1,5 +0,0 @@
-$newGrade=4.0;
-$currentGrade=3.4;
-$otherGrade=2*$newGrade-$currentGrade;
-print $otherGrade;
-$_=<STDIN>;
